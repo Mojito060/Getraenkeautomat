e4ab9564829de42290c3d6da42a73c43b2f184f0
Mojito060 <leanderwagener@gmail.com>
2022-07-03 14:16:46 +0200

    User panel basic function, Admin panel basic functions

--- main.py
+++ main.py
@@ -8,24 +8,27 @@ cur.execute('''CREATE TABLE IF NOT EXISTS getraenke
                 (name text, preis text, bestand text)''')
 con.commit()
 
-
-def display():
-    with sqlite3.connect("getraenke.db") as db:
-        c = db.cursor()
-        c.execute("CREATE VIEW IF NOT EXISTS test_VIEW AS SELECT name, preis, bestand FROM data")
-        db.commit()
-        data_pd = pd.read_sql('SELECT * FROM test_VIEW', db)
-        print(data_pd)
-
-
 option = input("Was möchtest du machen? \n1: Getränk kaufen. \n2: Admin Panel")
 
 if option == "1":
     print("Getränkeautomat")
+    cur.execute("SELECT * FROM getraenke")
+    print(cur.fetchall())
+    inputAuswahl = input("Welches Getränk willst du? \n Name:")
+    sql = '''SELECT * FROM getraenke WHERE name=\"''' + inputAuswahl + "\";"
+    for row in cur.execute(sql):
+        print(row)
+    print("Dein Getränk wird ausgegeben")
 
-if option == "2":
+
+elif option == "2":
     print("Admin panel")
-
+    getraenkeName = input("Wie heißt das Getränk?")
+    getraenkePreis = input("Wie viel kostet es?")
+    getraenkeAnzahl = input("Wie viele Getränke werden nachgefüllt?")
+    cur.execute("INSERT INTO getraenke (name, preis, bestand)VALUES (?, ?, ?)",
+                (getraenkeName, getraenkePreis, getraenkeAnzahl))
+    con.commit()
 
 else:
     print("Ungültige eingabe")
